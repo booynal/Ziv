002469b77afdd91e2d3461b678f62cffa260f2c3
booynal <home@test.com>
2015-10-16 00:18:58 +0800

    添加2013年的考试答案

---
+++ tutorials/test.refactoring/src/main/java/com/booynal/test/refactoring/gct/year2013/Year2013.java
@@ -0,0 +1,21 @@
+package com.booynal.test.refactoring.gct.year2013;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.booynal.test.refactoring.gct.GctScoreHandler;
+
+public class Year2013 {
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		GctScoreHandler handler = new GctScoreHandler();
+		List<String> standardAnswers = Files.readAllLines(Paths.get(Year2013.class.getResource("0").toURI()), Charset.defaultCharset());
+		List<String> myAnswers = Files.readAllLines(Paths.get(Year2013.class.getResource("1").toURI()), Charset.defaultCharset());
+		handler.calc(standardAnswers, myAnswers, GctScoreHandler.WEIGHT);
+	}
+
+}
