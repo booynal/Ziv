4314c2abd2131133c321521617ee8ab37f41315d
booynal <home@test.com>
2015-10-16 00:40:50 +0800

    添加个人所得税的表格

--- misc/misc.personal.income.tax/src/main/java/misc/personal/income/tax/Main.java
+++ misc/misc.personal.income.tax/src/main/java/misc/personal/income/tax/Main.java
@@ -6,6 +6,21 @@ public class Main {
 	public static void main(String[] args) {
 		double calc = new TaxRateHandler().calc(10329);
 		System.out.println(calc);
+//		print();
+		
+	}
+	
+	
+	public static void print() {
+		TaxRateHandler taxRateHandler = new TaxRateHandler();
+		StringBuilder sb = new StringBuilder();
+		for (int base = 1000; base < 100000; base += 1000) {
+			double calc = taxRateHandler.calc(base);
+			sb.append(base + "," + calc + "\r\n");
+		}
+		System.out.println("=======================================");
+		System.out.println(sb.toString());
+		
 	}
 
 }
