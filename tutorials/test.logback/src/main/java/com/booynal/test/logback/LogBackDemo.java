dd24c97f21caff02a80993a76de421dfbd620840
张黄江 <huangjiang.zhang@pcitc.com>
2016-10-28 21:19:54 +0800

    清理logback测试项目

--- tutorials/test.logback/src/main/java/com/booynal/test/logback/LogBackDemo.java
+++ tutorials/test.logback/src/main/java/com/booynal/test/logback/LogBackDemo.java
@@ -3,19 +3,22 @@ package com.booynal.test.logback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
 public class LogBackDemo {
 
 	private static final Logger logger = LoggerFactory.getLogger(LogBackDemo.class);
-	
+
 	public static void main(String[] args) {
 		// 以下语句将会打印出Logger的内部状态信息
-//		StatusPrinter.print((LoggerContext) LoggerFactory.getILoggerFactory());
+		StatusPrinter.print((LoggerContext) LoggerFactory.getILoggerFactory());
 		logger.trace("跟踪");
 		logger.debug("调试");
 		logger.info("信息");
 		logger.warn("警告");
 		logger.error("错误");
-		
+
 	}
-	
+
 }
