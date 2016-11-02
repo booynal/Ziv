18a1625cdb1ef7e6bc1bd89a0ac54de8bb5df2a4
张黄江 <huangjiang.zhang@pcitc.com>
2016-11-02 13:49:04 +0800

    修改spring.aop项目中单元测试的Assert包

--- misc/mvn.spring.aop/src/test/java/mvn/spring/aop/annotation/AopAnnotationTest.java
+++ misc/mvn.spring.aop/src/test/java/mvn/spring/aop/annotation/AopAnnotationTest.java
@@ -1,7 +1,6 @@
 package mvn.spring.aop.annotation;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
