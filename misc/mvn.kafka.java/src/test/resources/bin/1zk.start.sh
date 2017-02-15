891cbdb28edd5b3150f242c54d30271e3f7edbd8
张黄江 <huangjiang.zhang@pcitc.com>
2017-02-15 10:24:08 +0800

    修复：引用标准流的用法应该添加字符&
    Signed-off-by: 张黄江 <huangjiang.zhang@pcitc.com>

--- misc/mvn.kafka.java/src/test/resources/bin/1zk.start.sh
+++ misc/mvn.kafka.java/src/test/resources/bin/1zk.start.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >$KAFKA_HOME/logs/zk.log 2>1 &
-
-
+$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >$KAFKA_HOME/logs/zk.log 2>&1 &
--- misc/mvn.kafka.java/src/test/resources/bin/2ks.start.sh
+++ misc/mvn.kafka.java/src/test/resources/bin/2ks.start.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > $KAFKA_HOME/logs/ks.log 2>1 &
-
-
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > $KAFKA_HOME/logs/ks.log 2>&1 &
