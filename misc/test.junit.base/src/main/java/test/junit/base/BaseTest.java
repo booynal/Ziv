dff3c615024d9f0a8d3597f5243ae57df225b1ed
张黄江 <huangjiang.zhang@pcitc.com>
2017-03-27 00:15:18 +0800

    修改BaseTest：封装syserr
    Signed-off-by: 张黄江 <huangjiang.zhang@pcitc.com>

--- misc/test.junit.base/src/main/java/test/junit/base/BaseTest.java
+++ misc/test.junit.base/src/main/java/test/junit/base/BaseTest.java
@@ -1,26 +1,53 @@
 package test.junit.base;
 
-import java.io.PrintStream;
-
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.PrintStream;
 
 public class BaseTest {
 
+	protected static boolean enableThreadName = true;
+	protected static boolean enableFileName = true;
+
+	private static int maxThreadNameLength;
+	private static int maxFileNameLength;
+
 	@BeforeClass
 	public static void setUpSystemOut() {
-		System.setOut(new PrintStream(System.out) {
+		System.setOut(warp(System.out));
+		System.setErr(warp(System.err));
+	}
+
+	private static PrintStream warp(final PrintStream printStream) {
+		return new PrintStream(printStream) {
 
 			private String getFormatedString(Object x) {
-				StackTraceElement[] stackTrace = new Exception().getStackTrace();
-				int lineNumber = -1;
-				String fileName = null;
-				int depth = 2;
-				for (int i = depth; -1 == lineNumber; i++) {
-					lineNumber = stackTrace[i].getLineNumber();
-					fileName = stackTrace[i].getFileName();
+				StringBuilder sb = new StringBuilder();
+				if (enableThreadName) {
+					String threadName = Thread.currentThread().getName();
+					maxThreadNameLength = Math.max(maxThreadNameLength, threadName.length());
+					sb.append('[');
+					sb.append(String.format("%" + maxThreadNameLength + "s", threadName));
+					sb.append("]\t");
 				}
-				String threadName = Thread.currentThread().getName();
-				return String.format("[%s]\t(%s:%s)\t%s", threadName, fileName, lineNumber, x);
+				if (enableFileName) {
+					StackTraceElement[] stackTrace = new Exception().getStackTrace();
+					int lineNumber = -1;
+					String fileName = null;
+					int depth = 2;
+					for (int i = depth; -1 == lineNumber; i++) {
+						lineNumber = stackTrace[i].getLineNumber();
+						fileName = stackTrace[i].getFileName();
+					}
+					String target = fileName + ":" + lineNumber;
+					maxFileNameLength = Math.max(maxFileNameLength, target.length());
+					sb.append('(');
+					sb.append(String.format("%" + maxFileNameLength + "s", target));
+					sb.append(")\t");
+				}
+				sb.append(x);
+				return sb.toString();
 			}
 
 			@Override
@@ -52,7 +79,7 @@ public class BaseTest {
 			public void println(Object x) {
 				super.println(getFormatedString(x));
 			}
-		});
+		};
 	}
 
 }
